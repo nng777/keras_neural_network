059cc32d05e5d2db34bde458e4c8d83e2df36825
nng777 <nikodemusng@gmail.com>
2025-08-01 22:03:09 +0700

    Commit

--- task.py
+++
@@ -1,6 +0,0 @@
-"""
-TASK
-Make the following three changes to app.py:
-1.Add a Layer: Add a second Dense hidden layer to the model. It should have 8 neurons and use the relu activation function. Place it between the existing hidden layer and the output layer.
-2.Change the Optimizer: In the model.compile() step, change the optimizer from 'adam' to 'sgd' (Stochastic Gradient Descent).
-3.Increase Training Time: In the model.fit() step, increase the number of epochs from 50 to 100."""
